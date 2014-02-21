2baf0a3c6d5e0f5213f4098eec1b3ce5026085c1
Jonas Ghyllebert <jghyllebert@nuvia.io>
2014-02-21 23:34:36 +0100

    back to original

--- blog/blog/settings/production.py
+++ blog/blog/settings/production.py
@@ -15,14 +15,13 @@ DATABASES = {
 
 WSGI_APPLICATION = 'blog.prod_wsgi.application'
 
-STATIC_ROOT = "/home/ghyllebert/webapps/blog/repo/blog/static/"
 
-EMAIL_HOST = 'smtp.webfaction.com'
-EMAIL_HOST_USER = 'ghyllebert'
-EMAIL_HOST_PASSWORD = os.environ['MAILBOX_PASS']
-DEFAULT_FROM_EMAIL = 'jonas@ghyllebert.be'
-SERVER_EMAIL = 'info@ghyllebert.be'
+#EMAIL_HOST = 'smtp.webfaction.com'
+#EMAIL_HOST_USER = 'ghyllebert'
+#EMAIL_HOST_PASSWORD = os.environ['MAILBOX_PASS']
+#DEFAULT_FROM_EMAIL = 'jonas@ghyllebert.be'
+#SERVER_EMAIL = 'info@ghyllebert.be'
 
-ADMINS = ('jonas@ghyllebert.be',)
-MANAGERS = ADMINS
+#ADMINS = ('jonas@ghyllebert.be',)
+#MANAGERS = ADMINS
 
