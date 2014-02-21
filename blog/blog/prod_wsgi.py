0f355298ca581480ccaffa54fa2561d5dedd18e2
Jonas Ghyllebert <jghyllebert@nuvia.io>
2014-02-21 01:39:51 +0100

    edited prod_wsgi

--- blog/blog/prod_wsgi.py
+++ blog/blog/prod_wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ["DJANGO_SETTINGS_MODULE"])
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
